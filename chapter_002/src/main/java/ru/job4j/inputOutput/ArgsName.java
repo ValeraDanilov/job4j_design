67730eb98187bc590133cf9c755d897badc6f4e5
delphiets <delfiyets@gmail.com>
2020-12-28 18:05:35 +0200

    5.1. Именованные аргументы [#324620]

---
+++ chapter_002/src/main/java/ru/job4j/inputOutput/ArgsName.java
@@ -0,0 +1,41 @@
+package ru.job4j.inputOutput;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Arrays is null");
+        }
+        for (String element : args) {
+            if (!element.startsWith("-") && element.contains("=")) {
+                throw new IllegalArgumentException("The string is not correct");
+            }
+            String key = element.substring(element.indexOf('-') + 1, element.indexOf("="));
+            String value = element.substring(element.indexOf('=') + 1);
+            this.values.put(key, value);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/inputOutput/ArgsNameTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.inputOutput;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ArgsNameTest {
+
+    @Before
+    public void setUp() {
+        System.out.println("Before method");
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("After method");
+    }
+
+    @Test
+    public void whenGetFirst() {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        assertThat(jvm.get("Xmx"), is("512"));
+    }
+
+    @Test
+    public void whenGetFirstReorder() {
+        ArgsName jvm = ArgsName.of(new String[] {"-encoding=UTF-8", "-Xmx=512"});
+        assertThat(jvm.get("Xmx"), is("512"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenGetNotExist() {
+        ArgsName jvm = ArgsName.of(new String[] {});
+        jvm.get("Xmx");
+    }
+}
