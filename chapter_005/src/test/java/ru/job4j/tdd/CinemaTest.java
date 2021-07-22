b121f00820928e81e4c06b21cf2f93f200f341e5
delphiets <delfiyets@gmail.com>
2021-07-22 23:25:02 +0300

    2. Что такое TDD? [#4918]

---
+++ chapter_005/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.tdd;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CinemaTest {
+
+    @Before
+    public void before() {
+        System.out.println("Before method");
+    }
+
+    @After
+    public void after() {
+        System.out.println("After method");
+    }
+
+    @Test
+    public void testBuy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test(expected = Exception.class)
+    public void testBuyErrorDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.NOVEMBER, 33, 23, 0);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    public void testBuyErrorSeat() {
+        Account account = new AccountCinema();
+        Account newAccount = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
+        Ticket newTicket = cinema.buy(account, 1, 1, date);
+        Ticket ticket = cinema.buy(newAccount, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    public void testFindTrue() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Test
+    public void testFindFalse() {
+        Cinema cinema = new Cinema3D();
+        List<Session> sessions = cinema.find(session -> false);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+}
